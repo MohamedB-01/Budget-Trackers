a1c5d431c05e9fd709faba07f0a1b0392344f060
MohamedB <m.m.bassiouni@gmail.com>
2021-08-17 12:37:06 -0700

    updated webpack.config.js

---
+++ public/webpack.config.js
@@ -0,0 +1,44 @@
+const WebpackPwaManifest = require('webpack-pwa-manifest');
+const path = require('path');
+
+const config = {
+  entry: './assets/js/index.js' ,
+  output: {
+    path: __dirname + '/dist',
+    filename: '[name].bundle.js',
+  },
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+    ],
+  },
+  plugins: [
+    new WebpackPwaManifest({
+      fingerprints: false,
+      name: 'Budget Tracker Application',
+      short_name: 'Budget_App',
+      description: 'An application that allows you to track budget.',
+      background_color: '#01579b',
+      theme_color: '#ffffff',
+      'theme-color': '#ffffff',
+      start_url: '/',
+      icons: [
+        {
+          src: path.resolve('./assets/icons/icon-192x192.png'),
+          sizes: [192,  512],
+          destination: path.join('assets', 'icons'),
+        },
+      ],
+    }),
+  ],
+};
